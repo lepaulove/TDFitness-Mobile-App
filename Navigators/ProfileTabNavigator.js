cfe0be120e963800cf88364db0680092fba7cd78
lepaulove <lepaul.love@gmail.com>
2022-05-21 19:23:10 -0500

    Styling changes to profile screen header

--- Navigators/ProfileTabNavigator.js
+++ Navigators/ProfileTabNavigator.js
@@ -23,10 +23,10 @@ export default function ProfileTabNavigator({navigation}) {
 
 return (
    <>
-         <View style={{alignItems: 'center', justifyContent: 'center', backgroundColor:globalStyles.colors.mainColor, paddingTop:Platform.OS === 'ios' ? 10 : 10, marginVertical: Platform.OS === 'ios' ? 100 : 75}}>
+         <View style={{alignItems: 'center', paddingTop:Platform.OS === 'ios' ? 140 : 110, backgroundColor:globalStyles.colors.mainColor, height: Platform.OS === 'ios' ? 350 : 350}}>
                 <Image  source={{uri:'https://scontent-atl3-2.cdninstagram.com/v/t51.2885-15/279394209_544956843639076_6293183354528779495_n.jpg?stp=dst-jpg_e35_s1080x1080&cb=9ad74b5e-88ad7ee8&_nc_ht=scontent-atl3-2.cdninstagram.com&_nc_cat=104&_nc_ohc=hsAkEON8f_gAX_7DIF-&edm=ALQROFkBAAAA&ccb=7-4&ig_cache_key=MjgyNzE1MjgxOTkwNDkxMzI3MQ%3D%3D.2-ccb7-4&oh=00_AT8YsgPVNOU4mpPpTofR8cwNHD2yOSbvdCks5XKYm1p3gw&oe=628C4DE0&_nc_sid=30a2ef'}} 
-                        style={{height:150, width: 150, borderRadius:100, marginBottom:0}}/>
-                <Text style={{color:globalStyles.colors.black, fontSize:24, fontWeight:'700', paddingBottom: 40}}>Welcome Back {currentUser.displayName}!</Text>
+                        style={{height:150, width: 150, borderRadius:100}}/>
+                <Text style={{color:globalStyles.colors.black, fontSize:24, fontWeight:'700', paddingVertical:10}}>Welcome Back {currentUser.displayName}!</Text>
       </View>
       <Tab.Navigator>
       <Tab.Screen name='HISTORY' options={{
--- components/AppWrapper.js
+++ components/AppWrapper.js
@@ -1,14 +1,10 @@
 import 'react-native-reanimated';
 import 'react-native-gesture-handler';
-import { View, StatusBar } from 'react-native';
 import { useSelector } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 
 import UserDrawer from './UserDrawer';
-import NonUserDrawer from './NonUserDrawer';
-import globalStyles from '../styles/global.styles';
 import HomeStackNavigator from '../Navigators/HomeStackNavigator';
-import LoginScreen from '../screens/Login';
 
 const mapState = ({user}) => ({
   currentUser: user.currentUser
@@ -20,14 +16,7 @@ const { currentUser } = useSelector(mapState)
   return (
     
     <NavigationContainer>
-      {!currentUser ? <HomeStackNavigator /> : <UserDrawer />}
-      {/* <HomeStackNavigator /> */}
+      {currentUser ? <UserDrawer /> : <HomeStackNavigator />}
     </NavigationContainer>
-      // <View style={{flex:1}}>
-      // <StatusBar
-      //   backgroundColor={globalStyles.colors.black} 
-      //   />
-      // {currentUser ? <UserDrawer/> : <NonUserDrawer/>}
-      // </View>
   );
 }
